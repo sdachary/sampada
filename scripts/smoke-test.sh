5450497589fb0fed35c23b757b9355a81f04c26c
deepak <deepak@acharylab.dev>
2026-09-22 09:05:25 +0530

    scripts: smoke-test default API https://api.vayalabs.in

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set +H 2>/dev/null  # disable bash ! history expansion
-API="${API:-http://acharylab.140.245.227.176.nip.io}"
+API="${API:-https://api.vayalabs.in}"
 FRONTEND="${FRONTEND:-https://sampada.pages.dev}"
 EMAIL="${EMAIL:-demo@sampada.app}"
 PASS="${PASS:-demo123!}"
